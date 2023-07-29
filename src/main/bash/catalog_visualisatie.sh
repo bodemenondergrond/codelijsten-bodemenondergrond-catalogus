a352fcb08d4646bdcf2854172559aa43a17c4ef1
gezever <gezever@skynet.be>
2023-07-29 12:09:42 +0200

    catalog visualisatie

--- src/main/bash/catalog_visualisatie.sh
+++ src/main/bash/catalog_visualisatie.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sparql --data=../resources/be/vlaanderen/bodemenondergrond/data/id/catalog/codelijsten-bodemenondergrond-catalogus/catalog.ttl --query ../sparql/catalog_visualisatie.sparql --results=TURTLE | rdf2dot  | dot -Tpng > ../sparql/catalog_visualisatie.png
+#sparql --data=../resources/be/vlaanderen/bodemenondergrond/data/id/catalog/codelijsten-bodemenondergrond-catalogus/catalog.ttl --query ../sparql/catalog_visualisatie.sparql --results=TURTLE | rdf2dot  | dot -Tpng > ../sparql/catalog_visualisatie.png
 pushd  ../../..
 git pull
 mvn clean install
